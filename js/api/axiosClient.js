1054b29c84944daf3638db2919e5666b7c67676a
Nam3001 <nguyenvn30012003@gmail.com>
2023-04-30 09:14:01 +0700

    update baseURL

--- js/api/axiosClient.js
+++ js/api/axiosClient.js
@@ -1,27 +1,33 @@
-import axios from "axios";
+import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: 'https://fake-post-api.herokuapp.com/api',
+  baseURL: 'https://fake-post-ui.onrender.com/api',
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+  },
+})
+
+axiosClient.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem('accessToken')
+    if (accessToken) {
+      config.headers.Authorization = 'Bearer' + accessToken
+    }
+
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
   }
-})
+)
 
-axiosClient.interceptors.request.use(config => {
-  const accessToken = localStorage.getItem('accessToken')
-  if (accessToken) {
-    config.headers.Authorization = 'Bearer' + accessToken
+axiosClient.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    return Promise.reject(error)
   }
-
-  return config
-}, error => {
-  return Promise.reject(error)
-})
-
-axiosClient.interceptors.response.use(response => {
-  return response
-}, error => {
-  return Promise.reject(error)
-})
+)
 
 export default axiosClient
